3bf4ff10553c6ec3ffadae1a930976c453405df8
HeGuerrero <60445258+HeGuerrero@users.noreply.github.com>
2020-01-30 20:35:20 -0600

    Add files via upload

---
+++ index_box.js
@@ -0,0 +1,20 @@
+(function() { 
+  let template = document.createElement("template");
+  template.innerHTML = `  
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>amCharts V4 Example - simple-3D-pie-chart</title>
+    <link rel="stylesheet" href="index.css" />
+  </head>
+  <body>
+    <div id="chartdiv"></div>
+    <script src="../../../core.js"></script>
+    <script src="../../../charts.js"></script>
+    <script src="../../../themes/animated.js"></script>
+    <script src="index.js"></script>
+  </body>
+</html>
+ `;
+ 
+})();
